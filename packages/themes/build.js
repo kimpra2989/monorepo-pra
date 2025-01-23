0c469a7e8c445630d829191a26007e9682c511fa
kimpra <dustmqaksdltkfrlfdldi@gmail.com>
2025-01-24 00:30:51 +0900

    fix: esbuild script 보완

--- packages/themes/build.js
+++ packages/themes/build.js
@@ -1,6 +1,12 @@
 import * as esbuild from 'esbuild'
 import pkg from './package.json' assert { type: 'json' }
 
+const args = process.argv.slice(2)
+
+const dev = args.includes('--dev')
+const minify = !dev
+const watch = args.includes('--watch')
+
 // 라이브러리의 번들에서 제외할 속성들
 const external = Object.keys({
   ...pkg.devDependencies,
@@ -10,7 +16,7 @@ const external = Object.keys({
 const baseConfig = {
   entryPoints: ['src/index.ts'],
   bundle: true,
-  minify: true,
+  minify,
   sourcemap: true,
   outdir: 'dist',
   external,
@@ -32,9 +38,10 @@ Promise.all([
   .then(async (ctxs) => {
     console.log('build success')
 
-    await Promise.all(ctxs.map((ctx) => ctx.watch()))
-
-    console.log('watching')
+    if (watch) {
+      await Promise.all(ctxs.map((ctx) => ctx.watch()))
+      console.log('watching')
+    }
   })
   .catch((e) => {
     console.error('Build failed', e)
